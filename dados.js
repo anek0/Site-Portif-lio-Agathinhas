d4d419896f8061e0aebf2b4575573a891abc53e4
Agatha Camargo <136628536+anek0@users.noreply.github.com>
2024-09-07 10:06:10 -0300

    commit 1
    Incluindo os arquivos.

---
+++ dados.js
@@ -0,0 +1,34 @@
+let dados = [
+              {
+              titulo: "Sculpting - Blender",
+              descricao: "Minha jornada na criação digital começou com o bacharelado em Jogos Digitais, onde descobri a magia do sculpting no Blender. Fascinada pela possibilidade de moldar formas tridimensionais, mergulhei de cabeça nessa técnica, explorando desde esculturas realistas até personagens estilizados. Com o sculpting, posso dar vida às minhas ideias de forma mais livre e intuitiva, combinando minha paixão por arte com a tecnologia. Através da criação de personagens, cenários e objetos, busco aprimorar minhas habilidades e expandir meus conhecimentos em modelagem 3D, com o objetivo de trabalhar em projetos de jogos e animação.",
+             link : "https://www.artstation.com/agathacamargo9",
+             linkSite :"https://www.blender.org/features/sculpting/" ,
+             tags : "escultura  modelagem character personagem cenário 3D design"
+               }, 
+
+               {
+                titulo: "Modelagem 3D no Blender",
+                descricao:  'Exploração da criação de formas tridimensionais no Blender. Comecei modelando itens básicos e estou sigo estudando para conquistar a habilidade de desenvolver personagens e cenários. Atualmente, estou explorando a criação de ambientes para Jogos 3D - em consequência de minha formação.',
+                link: "https://www.artstation.com/agathacamargo9",
+                linkSite: "https://docs.blender.org/manual/pt/dev/modeling/index.html",
+                tags : "blender modelagem character personagem cenário 3D design"
+               },
+
+               {
+                titulo: "Ilustração Digital",
+                descricao: "A ilustração digital sempre foi minha área almejada, a arte de desenhar e colorir é minha paixão! Tenho conhecimento de softwares como Adobe Photoshop e Illustrator. Adoro criar personagens expressivos utilizando a lápis quanto as técnicas de pintura digital.",
+                link:  "https://www.artstation.com/agathacamargo9",
+                tags : "Photoshop Illutrator Adobe Paint Arte Digital design"
+               },
+
+               { 
+                titulo: "Pixel Art no Aseprite",
+                descricao: 'O mundo pixelado me encanta! No Aseprite, crio artes em pixels. Meus estudos sobre Pixel Art inicia-se no ano de inclusão no Bacharelado de Jogos Digitais e é tão incrível como poucos pixels podem transmitir tantas emoções.',
+                link: "https://www.artstation.com/agathacamargo9",
+                linkSite: "https://www.aseprite.org/",
+                tags:  "pixel quadrado pixilart piskel design "
+               }
+
+           ];
+
